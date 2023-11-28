6e6251c10bad5734d92f057a3c744dc33a7b6306
Egor <egor_chek@mail.ru>
2023-11-28 18:52:52 +0400

    добавленны футеры и хэдеры и изменено количество элементов в строке на 2

---
+++ AlphabetYandexPracticum/SupplementaryView.swift
@@ -0,0 +1,29 @@
+//
+//  SupplementaryView.swift
+//  AlphabetYandexPracticum
+//
+//  Created by admin on 28.11.2023.
+//
+
+import UIKit
+
+final class SupplementaryView: UICollectionReusableView {
+    let titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+                    titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+                    titleLabel.topAnchor.constraint(equalTo: topAnchor),
+                    titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+                ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- AlphabetYandexPracticum/ViewController.swift
+++ AlphabetYandexPracticum/ViewController.swift
@@ -33,8 +33,11 @@ final class ViewController: UIViewController {
     private func setupView() {
         collectionView.register(LetterCollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
         view.addSubview(collectionView)
+        collectionView.register(SupplementaryView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
+        collectionView.register(SupplementaryView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "footer")
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
     private func setupConstraints() {
@@ -60,6 +63,54 @@ extension ViewController: UICollectionViewDataSource {
             print("Не получилось создать ячейку")
             return UICollectionViewCell() }
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        var id: String
+        
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            id = "header"
+        case UICollectionView.elementKindSectionFooter:
+            id = "footer"
+        default:
+            id = ""
+        }
+        
+        let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                   withReuseIdentifier: id,
+                                                                   for: indexPath) as! SupplementaryView
+        view.titleLabel.text = "Здесь находиться SupplementView"
+        return view
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    //задается размер для хэдера в секции
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let indexPath = IndexPath(row: 0, section: section)
+        let headerView = self.collectionView(collectionView, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader, at: indexPath)
+        
+        return headerView.systemLayoutSizeFitting(CGSize(width: collectionView.frame.width, height: collectionView.frame.height), withHorizontalFittingPriority: .required, verticalFittingPriority: .fittingSizeLevel)
+    }
+    
+    //задается размер для футера в секции
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        let indexPath = IndexPath(row: 0, section: section)
+        let footerView = self.collectionView(collectionView, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionFooter, at: indexPath)
+        
+        return footerView.systemLayoutSizeFitting(CGSize(width: collectionView.frame.width, height: collectionView.frame.height), withHorizontalFittingPriority: .required, verticalFittingPriority: .fittingSizeLevel)
+    }
+    
+    // задается рамер ячейки
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width / 2, height: 50)
+    }
+    
+    //задаеться растояние между ячейками
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
 
